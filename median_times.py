1fc90e6268666bf4a4aadc8a439bbd0a9b4401f9
akeryilmaz <akerylm@gmail.com>
2019-02-17 22:22:51 +0200

    Median calculation implemented

--- median_times.py
+++ median_times.py
@@ -16,26 +16,52 @@ def ReadLocation(location):
             line_count += 1
     return location_ids
 
-def ReadPickupTimes(location_id):
-    '''reads csv file for a given location_id.
-    Input: Integer. location_id of a restaurant
-    Output: List of tuple of integers. (hour, pickup_time) location_id's of restaurants.'''
+def ReadPickupTimes(location_id, date):
+    '''reads csv file for a given location_id and given date.
+    Input: location_id: Integer. location_id of a restaurant
+            date: String. query date
+    Output: List of tuple of integers. (hour, pickup_time)'''
     pickup_times = []
     with open("pickup_times.csv", 'r') as csvFile:
         line_count = 0
         reader = csv.reader(csvFile)
         for row in reader:
             if line_count != 0:
-                if location_id == row[0]:
-                    pickup_times.append((int(row[1]), int(row[2])))
+                if location_id == int(row[0]) and date == row[1].split("T")[0]:
+                    pickup_times.append((int(row[1].split("T")[1].split(":")[0]), int(row[2])))
             line_count += 1
     return pickup_times
 
-def FindMedian():
+def FindMedian(pickup_times, hour_range):
+    '''finds median for given pickup times and hour range
+    Input: pickup_times: List of tuple of integers. (hour, pickup_time)
+           hour_range: Tuple of ıntegers. (lower bound, upper bound)
+    Output: Float. Median OR None if no pickup time exist'''
+    pickups = []
+    count = 0
+    for pickup_time in pickup_times:
+        # append only if order is in the given range
+        if pickup_time[0] >= hour_range[0] and pickup_time[0] < hour_range[1]:
+            pickups.append(pickup_time[1])
+            count += 1
+    if pickups == []:
+        return None
+    # Sort the pickup times to find median
+    pickups.sort()
+    # If there are even number of elements, median is the average of two elements in the middle
+    if count % 2 == 0:
+        return (pickups[int(count/2)] + pickups[int(count/2)-1])/2
+    # If there are odd number of elements, median is the element in the middle
+    else:
+        return float(pickups[int(count/2)])
 
 if __name__ == "__main__":
     arguments = sys.argv
-    location_ids = ReadLocation(arguments[0])
+    location_ids = ReadLocation(arguments[1])
+    hour_range = (int(arguments[3].split("-")[0]), int(arguments[3].split("-")[1]))
+    date = (arguments[2])
+    print("location_id , median")
     for location_id in location_ids:
-        pickup_times = ReadPickupTimes(location_id)
-        # TODO: Calculate median
+        pickup_times = ReadPickupTimes(location_id, date)
+        median = FindMedian(pickup_times, hour_range)
+        print (location_id, ",", median)
